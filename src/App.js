a5ae1c6e21d5699d4b4ed52cfa9598ceb5f1ccba
magda <soballamagda@gmail.com>
2020-10-30 10:31:13 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react';
+import './styles/App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    const [result, setResult] = useState('')
+    const inputRef = useRef(null)
+
+    useEffect(() => inputRef.current.focus())
+
+    const handleClick = (e) => {
+        setResult(result.concat(e.target.name))
+    }
+
+    const handleBackspace= () => {
+        setResult(result.slice(0, -1))
+    }
+
+    const handleClear = () => {
+        setResult('')
+    }
+
+    const handleCalculate = () =>{
+        try {
+            setResult(eval(result).toString())
+        }catch(error) {
+            setResult('Error')
+        }
+    }
+
+    return (
+        <div className='calc-app'>
+            <form>
+                <input type='text' value={result} ref={inputRef}/>
+            </form>
+            <div className="keypad">
+                <button id='clear' onClick={handleClear}>Clear</button>
+                <button id='backspace' onClick={handleBackspace}>C</button>
+                <button name='+' onClick={handleClick}>+</button>
+                <button name='7' onClick={handleClick}>7</button>
+                <button name='8' onClick={handleClick}>8</button>
+                <button name='9' onClick={handleClick}>9</button>
+                <button name='-' onClick={handleClick}>-</button>
+                <button name='4' onClick={handleClick}>4</button>
+                <button name='5' onClick={handleClick}>5</button>
+                <button name='6' onClick={handleClick}>6</button>
+                <button name='x' onClick={handleClick}>x</button>
+                <button name='1' onClick={handleClick}>1</button>
+                <button name='2' onClick={handleClick}>2</button>
+                <button name='3' onClick={handleClick}>3</button>
+                <button name='/' onClick={handleClick}>/</button>
+                <button name='0' onClick={handleClick}>0</button>
+                <button name='.' onClick={handleClick}>.</button>
+                <button id='result' onClick={handleCalculate}>Result</button>
+            </div>
+        </div>
+    )
+
 }
 
 export default App;
